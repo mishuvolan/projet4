a20501c8b2f4cb7c088930f52b442d1b5c3f3c13
juliette_jlttt <juliette.lofaro@gmail.com>
2018-01-14 11:35:40 +0100

    nn

---
+++ src/OC/ShopBundle/Webservice/Webservice.php
@@ -0,0 +1,51 @@
+<?php
+namespace OC\ShopBundle\Webservice;
+use Doctrine\ORM\EntityManager;
+
+class Webservice
+{
+    protected $em;
+    private $maxCapacity;
+    private $maxPurchaseItem;
+    public function __construct(EntityManager $entityManager, $maxCapacity, $maxPurchaseItem)
+    {
+        $this->em               = $entityManager;
+        $this->maxCapacity      = (int) $maxCapacity;
+        $this->maxPurchaseItem  = (int) $maxPurchaseItem;
+    }
+    /**
+     * 
+     *
+     * @param \DateTime $date
+     * @param $booking
+     * @return bool
+     */
+
+    public function checkCapacity(\DateTime $date, $booking)
+    {
+        $totalTicketForThisDate = $this->getNumberTicketFor($date);
+        return ( ($totalTicketForThisDate + $booking) > $this->maxCapacity ) ? false : true;
+    }
+    /**
+     *
+     *
+     * @param \DateTime $date
+     * @return int
+     */
+    public function getRemainingBooking(\DateTime $date)
+    {
+        $totalBookingForThisDate = $this->getNumberBookingFor($date);
+        return (int) ( ($this->maxCapacity - $totalBookingForThisDate) < $this->maxPurchaseItem ) ? ($this->maxCapacity - $totalBookingForThisDate) : $this->maxPurchaseItem;
+    }
+    /**
+     * 
+     *
+     * @param \DateTime $date
+     * @return mixed
+     */
+    private function getNumberTicketFor(\DateTime $date)
+    {
+        return $this->em->getRepository('ShopBundle:Booking')->getBookingFor($date);
+    }
+}
+?>
