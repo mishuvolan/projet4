27336c41f2325915b4c81baadf5589695546e0a1
juliette_jlttt <juliette.lofaro@gmail.com>
2018-01-14 11:29:17 +0100

    new

---
+++ src/OC/ShopBundle/Controller/WebServiceController.php
@@ -0,0 +1,34 @@
+<?php
+namespace OC\ShopBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class WebserviceController extends Controller
+{
+    /**
+     * 
+     *
+     * @param Request $request
+     * @return mixed
+     */
+
+    public function quotaAction(Request $request){
+        if ($request->isXmlHttpRequest()) {
+            $date                   = new \DateTime($request->get('date'));
+            $booking                = (int) $request->get('booking');
+            if($this->get('louvre_shop.webservice')->checkCapacity($date, $booking)){
+                $remainingPurchaseItemOnDate = $this->get('louvre_shop.webservice')->getRemainingbooking($date);
+                $response = ['availability' => true, 'remaining_purchase_item' => $remainingPurchaseItemOnDate ];
+            }else{
+                $response =  ['availability' => false];
+            }
+        }else{
+            $response = array(
+                'errorCode' => 400,
+                'errorMsg'  => 'Bad Request'
+            );
+        }
+        return new JsonResponse($response);
+    }
+}
